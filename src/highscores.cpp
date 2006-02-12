0f36f83b7d2833919629a9e3453995fdd6b165f9
Laurent Montel <montel@kde.org>
2006-02-12 09:52:39 +0000

    kurl--
    svn path=/trunk/KDE/kdegames/knetwalk/; revision=508564

--- src/highscores.cpp
+++ src/highscores.cpp
@@ -28,7 +28,7 @@ namespace KExtHighscore
 	{
 		setScoreType(Normal);
 		/*
-		setWWHighscores(KURL( HOMEPAGE ), VERSION);
+		setWWHighscores(KUrl( HOMEPAGE ), VERSION);
 		setShowStatistics(true);
 		const uint RANGE[16] = {    1, 3120, 3180, 3240, 3300, 3360, 3420, 3480,
 			3510, 3540, 3550, 3560, 3570, 3580, 3590, 3600  };
