dacda97ff6ce053f7d009eb912c3e9adffb3c3f0
Albert Astals Cid <tsdgeos@terra.es>
2006-12-20 20:19:57 +0000

    Fordwardport r615243 Set the histogram ranges, without them it crashes when trying to add a highscore
    svn path=/trunk/KDE/kdegames/knetwalk/; revision=615247

--- src/highscores.cpp
+++ src/highscores.cpp
@@ -30,12 +30,12 @@ namespace KExtHighscore
 		/*
 		setWWHighscores(KUrl( HOMEPAGE ), VERSION);
 		setShowStatistics(true);
-		const uint RANGE[16] = {    1, 3120, 3180, 3240, 3300, 3360, 3420, 3480,
-			3510, 3540, 3550, 3560, 3570, 3580, 3590, 3600  };
-		QMemArray<uint> s;
-		s.duplicate(RANGE, 16);
-		setScoreHistogram(s, ScoreBound);
-*/
+		*/
+		const uint RANGE[16] = { 0, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160 };
+		QVector<uint> s;
+		for(int i=0;i<16;++i)
+			s.append(RANGE[i]);
+		setScoreHistogram(s, ScoreNotBound);
 		//Item *item = new Item((uint)0, i18n("Clicks"), Qt::AlignRight);
 		//addScoreItem("nb_actions", item);
 	}
