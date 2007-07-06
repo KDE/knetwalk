033c64b2079b0618bed468dc3aaf181d03f06694
Albert Astals Cid <tsdgeos@terra.es>
2007-07-06 14:49:08 +0000

    add closing </p>
    svn path=/trunk/KDE/kdegames/knetwalk/; revision=684369

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -74,11 +74,11 @@ MainWindow::MainWindow(QWidget *parent)
 
 	this->setWhatsThis( i18n("<h3>Rules of Game</h3>"
 			"<p>You are the system administrator and your goal"
-			" is to connect each computer to the central server."
+			" is to connect each computer to the central server.</p>"
 			"<p>Click the right mouse's button for turning the cable"
 			" in a clockwise direction, and left mouse's button"
-			" for turning the cable in a counter-clockwise direction."
-			"<p>Start the LAN with as few turns as possible!"));
+			" for turning the cable in a counter-clockwise direction.</p>"
+			"<p>Start the LAN with as few turns as possible!</p>"));
 
 	//const int cellsize = KGlobal::iconLoader()->loadIcon("knetwalk/background.png", K3Icon::User, 32).width();
 	const int cellsize = 32;
