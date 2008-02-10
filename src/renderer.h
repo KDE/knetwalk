61bf0c692524aa7cde52141a8cad1431bed49812
Stefan Majewsky <majewsky@gmx.net>
2008-02-10 12:46:12 +0000

    Corrected Fela's mail address.
    svn path=/trunk/KDE/kdegames/knetwalk/; revision=773158

--- src/renderer.h
+++ src/renderer.h
@@ -1,6 +1,6 @@
 /*
     Copyright 2007 Dmitry Suzdalev <dimsuz@gmail.com>
-    Copyright 2007 Fela Winkelmolen <dimsuz@gmail.com>
+    Copyright 2007 Fela Winkelmolen <fela.kde@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
