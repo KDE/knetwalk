9136df387c49d00baff432093edfa8c8b05f7f74
Fela Winkelmolen <fela.kde@gmail.com>
2008-03-08 15:55:29 +0000

    Copyright notices updated and made consistent.
    svn path=/trunk/KDE/kdegames/knetwalk/; revision=783544

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,21 +1,21 @@
-/***************************************************************************
- *   Copyright (C) 2004, 2005 Andi Peredri <andi@ukr.net>                
- *   Copyright (C) 2007 Simon Hürlimann <simon.huerlimann@huerlisi.ch>   
- *                                                                         
- *   This program is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU General Public
- *   License as published by the Free Software Foundation; either
- *   version 2 of the License, or (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- ***************************************************************************/
+/*
+    Copyright 2004-2005 Andi Peredri <andi@ukr.net>   
+    Copyright 2007 Simon Hürlimann <simon.huerlimann@huerlisi.ch> 
+    Copyright 2007-2008 Fela Winkelmolen <fela.kde@gmail.com> 
+  
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "mainwindow.h"
 
